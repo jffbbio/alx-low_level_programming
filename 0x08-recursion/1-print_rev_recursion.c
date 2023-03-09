b27c80b55e95e269026398be993cdf3f6883c562
Your Name <you@example.com>
2023-03-09 06:33:01 -0800

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -3,13 +3,13 @@
  * _print_rev_recursion - print a string in reverse
  * @s: string
  * Return 0;
- */
+*/
 
 void _print_rev_recursion(char *s)
 {
 	if (*s)
 	{
-	_print_rev_recursion(s + 1);
-	_putchar(*s);
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
 }
